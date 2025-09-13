168dd9b6063b53b503a6d57cedb681482d57ef64
rickkcastro <rm564560@fiap.com.br>
2025-09-12 22:25:54 -0300

    Revert "feat: simplify user profile handling and remove complete profile route" Revert
    This reverts commit 1d0e1258bbfc5537af7e806d5ae12e8c42c50b3e.

--- src/main.jsx
+++ src/main.jsx
@@ -6,24 +6,26 @@ import { BrowserRouter, Route, Routes, Outlet } from "react-router-dom";
 import ProtectedRoute from "./routes/ProtectedRoute.jsx";
 import Login from "./pages/login/Login.jsx";
 import AppRoutes from "./routes/AppRoutes.jsx";
+import CompleteProfile from "./pages/completeProfile/CompleteProfile.jsx";
 
 createRoot(document.getElementById("root")).render(
-    <StrictMode>
-        <UserProvider>
-            <BrowserRouter>
-                <Routes>
-                    <Route
-                        element={
-                            <ProtectedRoute>
-                                <Outlet />
-                            </ProtectedRoute>
-                        }
-                    >
-                        <Route path="/login" element={<Login />} />
-                        <Route path="/*" element={<AppRoutes />} />
-                    </Route>
-                </Routes>
-            </BrowserRouter>
-        </UserProvider>
-    </StrictMode>
+  <StrictMode>
+    <UserProvider>
+      <BrowserRouter>
+        <Routes>
+          <Route
+            element={
+              <ProtectedRoute>
+                <Outlet />
+              </ProtectedRoute>
+            }
+          >
+            <Route path="/login" element={<Login />} />
+            <Route path="/complete-profile" element={<CompleteProfile />} />
+            <Route path="/*" element={<AppRoutes />} />
+          </Route>
+        </Routes>
+      </BrowserRouter>
+    </UserProvider>
+  </StrictMode>
 );
